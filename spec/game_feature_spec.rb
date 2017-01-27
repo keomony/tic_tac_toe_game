86329afe94c386e87cefa9bc075497c9086e044a
keomony khun <keomony@gmail.com>
2017-01-27 12:34:16 +0000

    Passes 'game should have 2 players'

--- spec/game_feature_spec.rb
+++ spec/game_feature_spec.rb
@@ -2,10 +2,10 @@ require './lib/game'
 
 describe "Tic Tac Toe Game" do
   let(:player1) { Player.new('X') }
-  let(:player2) { Player.new('Y') }
-  subject(:game) { described_class.new(player1,player2)}
+  let(:player2) { Player.new('O') }
+  subject(:game) { Game.new(player1,player2)}
 
   it "should have two players" do
-    expect(game).to respond_to(:new).with(2).arguments
+    expect(Game).to respond_to(:new).with(2).arguments
   end
 end
