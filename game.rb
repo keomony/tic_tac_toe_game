e038213dfb5f7358c2f3a0b39dd8b80c423797da
Keomony Khun <keomony@gmail.com>
2017-02-21 21:40:03 +0000

    Show Ascii grid on console

--- game.rb
+++ game.rb
@@ -1,1 +1,4 @@
 puts "Welcome to Tic Tac Toe Game!"
+3.times do
+  puts "| | | |"
+end
--- spec/features/game_feature_spec.rb
+++ spec/features/game_feature_spec.rb
@@ -3,4 +3,8 @@ describe "game features" do
     output = `ruby game.rb`
     expect(output).to include("Welcome to Tic Tac Toe Game!")
   end
+  it "should show 3 by 3 grid" do
+    output = `ruby game.rb`
+    expect(output).to include("| | | |\n| | | |\n| | | |")
+  end
 end
