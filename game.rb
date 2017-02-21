9639b4e58cacc1f4cfec8cda8edda5e4492ff08c
Keomony Khun <keomony@gmail.com>
2017-02-21 22:30:55 +0000

    First user makes a move

--- game.rb
+++ game.rb
@@ -1,4 +1,20 @@
 puts "Welcome to Tic Tac Toe Game!"
-3.times do
-  puts "| | | |"
+
+def show_the_grid(value = nil)
+  i = 0
+  3.times do
+    3.times do |j|
+      if i+j+1 == value
+        print "|X"
+      else
+        print "|#{i+j+1}"
+      end
+    end
+    print "|\n"
+    i +=3
+  end
 end
+
+show_the_grid
+value = gets.to_i
+show_the_grid(value)
--- spec/features/game_feature_spec.rb
+++ spec/features/game_feature_spec.rb
@@ -1,10 +1,52 @@
+require 'pty'
+
 describe "game features" do
+
   it "should start the game " do
-    output = `ruby game.rb`
-    expect(output).to include("Welcome to Tic Tac Toe Game!")
+    PTY.spawn('ruby game.rb') do |output, input|
+      expect(output.readline).to include("Welcome to Tic Tac Toe Game!")
+    end
   end
   it "should show 3 by 3 grid" do
-    output = `ruby game.rb`
-    expect(output).to include("| | | |\n| | | |\n| | | |")
+    PTY.spawn('ruby game.rb') do |output, input|
+      output.readline
+      expect(output.readline.chomp).to eq("|1|2|3|")
+      expect(output.readline.chomp).to eq("|4|5|6|")
+      expect(output.readline.chomp).to eq("|7|8|9|")
+    end
   end
+
+  it "first user makes a move" do
+    PTY.spawn('ruby game.rb') do |output, input|
+      4.times do
+        output.readline
+      end
+      input.puts "2"
+      output.readline
+      expect(output.readline.chomp).to eq("|1|X|3|")
+      expect(output.readline.chomp).to eq("|4|5|6|")
+      expect(output.readline.chomp).to eq("|7|8|9|")
+    end
+  end
+
+  xit "second user makes a move" do
+    PTY.spawn('ruby game.rb') do |output, input|
+      4.times do
+        output.readline
+      end
+      input.puts "2"
+      4.times do
+        output.readline
+      end
+      input.puts "6"
+      output.readline
+      expect(output.readline.chomp).to eq("|1|X|3|")
+      expect(output.readline.chomp).to eq("|4|5|O|")
+      expect(output.readline.chomp).to eq("|7|8|9|")
+    end
+  end
+  xit "user can not mark used square" do
+
+  end
+
 end
